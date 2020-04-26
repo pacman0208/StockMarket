f8d16b1f82c0da958b26503211b1bf46e9989034
qman23 <q-man23@163.com>
2020-04-26 14:20:44 +0800

    add router

--- frontEnd/src/app/app-routing.module.ts
+++ frontEnd/src/app/app-routing.module.ts
@@ -1,8 +1,16 @@
+//router configuration
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HeaderComponent } from './component/header/header.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { FooterComponent } from './component/footer/footer.component';
+import { LoginComponent } from './component/login/login.component';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  {path:"login",component:LoginComponent},
+  //if there's no path match, then redirect to this path
+  {path:"**",redirectTo:"login"}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
