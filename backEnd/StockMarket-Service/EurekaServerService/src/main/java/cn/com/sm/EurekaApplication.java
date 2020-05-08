07c7fc9c9c03de97d222d0bbb38ae0be9c2faef0
pacman0208 <lzwtq0208@gmail.com>
2020-05-08 17:33:03 +0800

    backend init

---
+++ backEnd/StockMarket-Service/EurekaServerService/src/main/java/cn/com/sm/EurekaApplication.java
@@ -0,0 +1,19 @@
+package cn.com.sm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * @Auther: Lee
+ * @Date: 5/8/2020 5:24 PM
+ * @Software: IntelliJ IDEA
+ * @Description:
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaApplication.class,args);
+    }
+}
---
+++ backEnd/StockMarket-Service/src/test/java/cn/com/sm/StockMarketServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.com.sm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockMarketServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
